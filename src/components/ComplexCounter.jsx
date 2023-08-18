c2c6e2cf68b8286ace3b003a58481412b4da75c2
Mr. Hasan <hasanshahriar32@gmail.com>
2023-08-18 13:02:57 +0600

    updated

--- src/components/ComplexCounter.jsx
+++ src/components/ComplexCounter.jsx
@@ -2,13 +2,18 @@ import { useReducer } from "react";
 
 const initialState = {
   counter: 0,
+  counter2: 100,
 };
 const reducer = (state, action) => {
   switch (action.type) {
     case "increment":
-      return { counter: state.counter + action.value };
+      return { ...state, counter: state.counter + action.value };
     case "decrement":
-      return { counter: state.counter - action.value };
+      return { ...state, counter: state.counter - action.value };
+    case "increment2":
+      return { ...state, counter2: state.counter2 + action.value };
+    case "decrement2":
+      return { ...state, counter2: state.counter2 - action.value };
 
     default:
       return state;
@@ -18,7 +23,7 @@ function ComplexCounter() {
   const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <>
-      <h1>Counter App: {count.counter}</h1>
+      <h1>Counter App 1: {count.counter}</h1>
       <button
         style={{ marginBottom: "2px" }}
         onClick={() => dispatch({ type: "increment", value: 1 })}
@@ -26,6 +31,16 @@ function ComplexCounter() {
         Increment by 1
       </button>
       <button onClick={() => dispatch({ type: "decrement", value: 2 })}>
+        Decrement by 2
+      </button>
+      <h1>Counter App 2: {count.counter2}</h1>
+      <button
+        style={{ marginBottom: "2px" }}
+        onClick={() => dispatch({ type: "increment2", value: 1 })}
+      >
+        Increment by 1
+      </button>
+      <button onClick={() => dispatch({ type: "decrement2", value: 2 })}>
         Decrement by 2
       </button>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
-      <ul>
+      <ul style={{}}>
         <li>
           <Link to="/simple">Simple Counter</Link>
         </li>
